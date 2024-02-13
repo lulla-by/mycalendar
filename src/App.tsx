253721e5be26a9e95c4492eda235d3410bbb566d
lulla-by <thfl3363@hanmail.net>
2024-02-13 19:35:55 +0900

    recoil 설치 및 theme 아톰 생성

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,17 @@ import { Fragment } from 'react';
 import Layout from './components/Layout';
 import Calendar from './components/Calendar';
 import MemoBoard from './components/MemoBoard';
+import { RecoilRoot } from 'recoil';
 
 function App() {
   return (
     <Fragment>
-      <Layout>
-        <Calendar />
-        <MemoBoard />
-      </Layout>
+      <RecoilRoot>
+        <Layout>
+          <Calendar />
+          <MemoBoard />
+        </Layout>
+      </RecoilRoot>
     </Fragment>
   );
 }
---
+++ src/store/theme.ts
@@ -0,0 +1,13 @@
+import { atom } from 'recoil';
+
+export interface IContentTypes {
+  name: string;
+}
+
+//recoil state 생성
+export const themeState = atom<IContentTypes>({
+  key: 'theme',
+  default: {
+    name: 'forestTheme',
+  },
+});
