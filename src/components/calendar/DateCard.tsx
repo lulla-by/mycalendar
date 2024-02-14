8cf2a247a8dcf6ce5ea9dedf50c9521da917dcf6
lulla-by <thfl3363@hanmail.net>
2024-02-14 16:07:38 +0900

    달력 컴포넌트 완성

--- src/components/calendar/DateCard.tsx
+++ src/components/calendar/DateCard.tsx
@@ -2,17 +2,54 @@ import React from 'react';
 import styled from 'styled-components';
 
 interface DateCard {
-  date: number;
+  props: {
+    date: number;
+    state: string;
+    today?: boolean;
+  };
 }
-const DateCard = ({date}: DateCard) => {
-  return <CardContainer>{date}</CardContainer>;
+
+type StyledProps = Pick<DateCard, 'props'>;
+
+const DateCard = ({ props }: DateCard) => {
+  return (
+    <CardContainer props={props}>
+      <DateText props={props}>{props.date}</DateText>
+    </CardContainer>
+  );
 };
 
 export default DateCard;
 
-const CardContainer = styled.div`
+const CardContainer = styled.div<StyledProps>`
   height: 100%;
   display: inline-block;
-  background-color:${({ theme }) => theme.colors.light};
-  border: 1px solid black;
+  background-color: ${({ props, theme }) =>
+    props.state === 'prev' || props.state === 'next'
+      ? '#f1f3f5'
+      : theme.colors.light};
+  border: 1px solid #e9ecef;
+
+  color: ${({ props, theme }) =>
+    props.state === 'prev' || props.state === 'next'
+      ? '#868e96'
+      : theme.colors.main};
+`;
+
+const DateText = styled.p<StyledProps>`
+display:block;
+  background-color: ${({ theme, props }) =>
+    props.state === 'prev' || props.state === 'next'
+      ? null
+      : theme.colors.median};
+  color: ${({ theme, props }) =>
+    props.state === 'prev' || props.state === 'next'
+      ? '#ced4da'
+      : theme.colors.base};
+  width: 30px;
+  height: 30px;
+  text-align: center;
+  line-height: 30px;
+  font-weight: 600;
+  border-radius: 0 0 5px 0;
 `;
