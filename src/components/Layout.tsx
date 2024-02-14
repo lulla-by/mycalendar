abaabdb434e201f6ffa44672a1d2915af0648fad
lulla-by <thfl3363@hanmail.net>
2024-02-14 12:16:32 +0900

    DateContext 생성 및 적용

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,13 +1,17 @@
-import { Fragment } from 'react';
+import { Fragment, createContext, useState } from 'react';
 import Header from './Header';
 import styled, { ThemeProvider } from 'styled-components';
 import { useRecoilValue } from 'recoil';
 import { themeState } from '../store/theme';
 import { forest, sea, pink } from '../styles/theme';
+import { DateContext } from '../context/DateContext';
 
 type LayoutProps = {
   children: React.ReactNode;
 };
+
+export const AppContext = createContext<number | undefined>(undefined);
+
 const Layout = (props: LayoutProps) => {
   const content = useRecoilValue(themeState);
   const getTheme = () => {
@@ -23,13 +27,29 @@ const Layout = (props: LayoutProps) => {
     }
   };
 
+  const nowDate = new Date();
+  const year = nowDate.getFullYear();
+  const month = nowDate.getMonth() + 1;
+  const [propsMonth, setPropsmonth] = useState(month);
+  const [propsYear, setPropYear] = useState(year);
+
+  const changeMonth = (num: number) => {
+    setPropsmonth(num);
+  };
+
+  const changeYear = (num: number) => {
+    setPropYear(num);
+  };
+
   const selectedTheme = getTheme();
   return (
     <Fragment>
       <ThemeProvider theme={selectedTheme}>
         <LayountContainer>
-          <Header />
-          <main>{props.children}</main>
+          <DateContext.Provider value={{ propsMonth, propsYear }}>
+            <Header onChangeMonth={changeMonth} onChangeYear={changeYear} />
+            <main>{props.children}</main>
+          </DateContext.Provider>
         </LayountContainer>
       </ThemeProvider>
     </Fragment>
@@ -41,4 +61,9 @@ export default Layout;
 const LayountContainer = styled.div`
   width: 1200px;
   margin: auto;
+border: 2px solid #dee2e6;
+border-radius: 5px;
+  main {
+    display: flex;
+  }
 `;
---
+++ src/context/DateContext.ts
@@ -0,0 +1,3 @@
+import { createContext } from 'react';
+
+export const DateContext = createContext({ propsMonth: 1, propsYear: 0 });
