83dff30ff82d255fb1ff597ef2cd755a63570cee
lulla-by <thfl3363@hanmail.net>
2024-02-14 12:18:36 +0900

    Header 컴포넌트 CSS 및 기능 추가 - 년도/ 월 렌더링 - prev, next 버튼으로 이전달 다음달로 변경 기능 구현 - css 수정예정

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,31 +1,95 @@
 import { useSetRecoilState } from 'recoil';
 import styled from 'styled-components';
 import { themeState } from '../store/theme';
-import Button from './Button';
+import ThemeButton from './ThemeButton';
+import { useContext } from 'react';
+import { DateContext } from '../context/DateContext';
 
-const Header = () => {
+interface HeaderProps {
+  onChangeMonth: (num: number) => void;
+  onChangeYear: (num: number) => void;
+}
+
+const Header = ({ onChangeMonth, onChangeYear }: HeaderProps) => {
   const setContent = useSetRecoilState(themeState);
 
   const changeHandelr = (type: string) => {
     setContent({ name: type });
   };
+
+  const { propsMonth, propsYear } = useContext(DateContext);
+
+  const onPlusHandler = () => {
+    if (propsMonth == 12) {
+      onChangeMonth(1);
+      onChangeYear(propsYear + 1);
+    } else {
+      onChangeMonth(propsMonth + 1);
+    }
+  };
+  const onMinusHandler = () => {
+    if (propsMonth == 1) {
+      onChangeMonth(12);
+      onChangeYear(propsYear - 1);
+    } else {
+      onChangeMonth(propsMonth - 1);
+    }
+  };
+
+  const monthArr: { [key: number]: string } = {
+    1: 'January',
+    2: 'February',
+    3: 'March',
+    4: 'April',
+    5: 'May',
+    6: 'June',
+    7: 'July',
+    8: 'August',
+    9: 'September',
+    10: '  October',
+    11: 'November',
+    12: 'December',
+  };
+
+  const month = monthArr[propsMonth];
+
   return (
     <HeaderContainer>
       <DateUl>
         <li>
-          <h2>Feb</h2>
+          <h2>
+            {month}, {propsYear}
+          </h2>
+        </li>
+        <li>
+          <MoveYearButton
+            onClick={() => {
+              onMinusHandler();
+            }}
+          >
+            prev
+          </MoveYearButton>
+        </li>
+        <li>
+          <MoveYearButton
+            onClick={() => {
+              onPlusHandler();
+            }}
+          >
+            next
+          </MoveYearButton>
         </li>
       </DateUl>
 
       <ThemeUL>
         <li>
-          <Button text="Forest" color="#12372A" clickEvent={changeHandelr} />
+          <ThemeButton text="Forest" color="#12372A" clickEvent={changeHandelr} />
         </li>
         <li>
-          <Button text="Sea" color="#78C1F3" clickEvent={changeHandelr} />
+          <ThemeButton text="Sea" color="#78C1F3" clickEvent={changeHandelr} />
         </li>
         <li>
-          <Button text="Pink" color="#FF78C4" clickEvent={changeHandelr} />
+          <ThemeButton text="Pink" color="#FF78C4" clickEvent={changeHandelr} />
         </li>
       </ThemeUL>
     </HeaderContainer>
@@ -41,12 +105,17 @@ const HeaderContainer = styled.header`
 `;
 const DateUl = styled.div`
   display: flex;
-  justify-content: center;
-  gap: 5px;
-  list-style:none;
+  gap: 10px;
+  list-style: none;
   font-size: 24px;
   font-weight: 600;
-`
+
+  li:first-child {
+    margin-left: 10px;
+    width: 200px;
+  }
+  align-items: center;
+`;
 
 const ThemeUL = styled.ul`
   display: flex;
@@ -55,3 +124,14 @@ const ThemeUL = styled.ul`
   right: 10px;
   gap: 10px;
 `;
+
+const MoveYearButton = styled.button`
+  background-color: white;
+  border: 1px solid #dee2e6;
+  border-radius: 5px;
+  width: 40px;
+  height: 20px;
+  padding:0;
+  margin: 0;
+  cursor: pointer;
+`
