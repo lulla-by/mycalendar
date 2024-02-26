5fd8395fd159d367fe7ec1c657dadc90e57c424e
lulla-by <thfl3363@hanmail.net>
2024-02-26 14:04:46 +0900

    Date.tsx 리팩토링 - getMonthDateArray 함수를 사용하여 배열 생성 방식 개선

--- src/components/calendar/Date.tsx
+++ src/components/calendar/Date.tsx
@@ -2,65 +2,56 @@ import { useContext } from 'react';
 import { DateContext } from '../../context/DateContext';
 import styled from 'styled-components';
 import DateCard from './DateCard';
-import { DateItem } from '../../types/types';
+
+const getMonthDateArray = (
+  start: number,
+  end: number,
+  state: 'prev' | 'current' | 'next',
+  year: number,
+  month: number
+) => {
+  const monthArray = [];
+  for (let i = start; i <= end; i++) {
+    monthArray.push({ state, date: i, year, month });
+  }
+  return monthArray;
+};
 
 const DateRender = () => {
   const { propsMonth: month, propsYear: year } = useContext(DateContext);
 
-  // 이번달 정보
-  const currentMonthArray: DateItem[] = [];
-  // 이번달 시작일
-  const currentMonthLast = new Date(year, month, 0);
-  const currentMonthLastDate = currentMonthLast.getDate();
+  const lastDateOfPreviousMonth = new Date(year, month - 1, 0).getDate();
+  const lastDateOfCurrentMonth = new Date(year, month, 0).getDate();
+  const firstDayOfCurrentMonth = new Date(year, month - 1, 1).getDay();
 
-  // 이번달 마지막일
-  const currentMonthFirst = new Date(year, month - 1, 1);
-  const currentMonthFirstDate = currentMonthFirst.getDay();
+  const prevMonthDateArray = getMonthDateArray(
+    lastDateOfPreviousMonth - firstDayOfCurrentMonth + 1,
+    lastDateOfPreviousMonth,
+    'prev',
+    year,
+    month - 1
+  );
 
-  // 이전달 정보
-  const prevMonthArray: DateItem[] = [];
-  // 이전달 마지막일
-  const previousMonthLast = new Date(year, month - 1, 0);
-  const previousMonthLastDate = previousMonthLast.getDate();
-  const previousMonthLastYear = previousMonthLast.getFullYear();
-  const previousMonthLastMonth = previousMonthLast.getMonth()+1;
-  
-  
-  
-  // 다음달 정보
-  const nextMonthLast = new Date(year, month + 1, 0);
-  const nextMonthLastYear = nextMonthLast.getFullYear();
-  const nextMonthLastMonth = nextMonthLast.getMonth()+1;
-  
-  const today = new Date().getDate();
+  const currentMonthDateArray = getMonthDateArray(
+    1,
+    lastDateOfCurrentMonth,
+    'current',
+    year,
+    month
+  );
 
-  for (
-    let i = previousMonthLastDate - currentMonthFirstDate + 1;
-    i <= previousMonthLastDate;
-    i++
-  ) {
-    prevMonthArray.push({ state: 'prev', date: i, year:previousMonthLastYear, month:previousMonthLastMonth});
-  }
+  const nextMonthDateArray = getMonthDateArray(
+    1,
+    7 - ((prevMonthDateArray.length + currentMonthDateArray.length) % 7),
+    'next',
+    year,
+    month + 1
+  );
 
-  for (let i = 1; i <= currentMonthLastDate; i++) {
-    today === i
-      ? currentMonthArray.push({ state: 'current', date: i, year, month })
-      : currentMonthArray.push({ state: 'current', date: i, year, month });
-  }
-
-  // 이전달과 이번달 배열 합치기
-
-  const newArr = [...prevMonthArray, ...currentMonthArray];
-
-  // 다음달 정보 추가
-  for (let i = 1; i < 7; i++) {
-    if (newArr.length % 7 !== 0) {
-      newArr.push({ state: 'next', date: i, year:nextMonthLastYear, month:nextMonthLastMonth });
-    }
-  }
+  const fullDateArray = [...prevMonthDateArray, ...currentMonthDateArray, ...nextMonthDateArray];
   return (
     <Container>
-      {newArr.map((date) => (
+      {fullDateArray.map((date) => (
         <DateCard
           key={`${date.year}/${date.month}/${date.date}`}
           props={date}
@@ -74,7 +65,7 @@ export default DateRender;
 
 const Container = styled.div`
   display: grid;
-  grid-template-columns:repeat(7, minmax(0, 1fr)); 
+  grid-template-columns: repeat(7, minmax(0, 1fr));
   grid-auto-rows: 1fr;
   height: 95%;
   gap: 2px;
